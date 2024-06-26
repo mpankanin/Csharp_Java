ab6a479a3df9cb45fc3fdb8fed46899769504b60
mpankanin <s24188@pjwstk.edu.pl>
2024-06-26 09:48:58 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+//Console.WriteLine("Hello, World!"); - its an equivalent of Java's System.out.println("Hello, World!");
+//C# beneath the scenes wraps the code in a class and a Main method
+
+class Program
+{
+    private string _name = "John";
+    //private fields convention in C# is to use an underscore before the field name
+
+    public string LastName { get; set; }
+    //this field is a property, and the convention in C# is to use PascalCase for properties
+    //this is a shorthand for creating a private field and a public accessor methods
+    //in Rider, you can use the shortcut "prop" and press tab twice to create a property
+
+    public static void Main()
+    {
+//        Method conventions in C# are PascalCase
+//                         In Java are camelCase
+        Console.WriteLine("Hello, World!");
+        
+    }
+    
+}
+
+
+
+
+
