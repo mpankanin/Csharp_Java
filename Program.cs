78ad206347b05f970b31dc9d39b744eefa045695
mpankanin <s24188@pjwstk.edu.pl>
2024-07-11 23:28:23 +0200

    basic stuff added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-//Console.WriteLine("Hello, World!"); - its an equivalent of Java's System.out.println("Hello, World!");
+﻿//Console.WriteLine("Hello, World!"); - its an equivalent of Java's System.out.println("Hello, World!");
 //C# beneath the scenes wraps the code in a class and a Main method
 
 class Program
@@ -12,17 +11,20 @@ class Program
     //this is a shorthand for creating a private field and a public accessor methods
     //in Rider, you can use the shortcut "prop" and press tab twice to create a property
 
+    private readonly string _middleName = "Doe";
+    //readonly fields are like final fields in Java, they can only be assigned once
+
     public static void Main()
     {
 //        Method conventions in C# are PascalCase
 //                         In Java are camelCase
         Console.WriteLine("Hello, World!");
+
+        //string is written with a lowercase s in C# but it's still an object
+        string name = "John";
+
+        //var is a keyword that tells the compiler to infer the type of the variable
+        var birthdate = DateTime.Now;
         
     }
-    
 }
-
-
-
-
-
